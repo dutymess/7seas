60455aed02c70f1565361e5675d60268e5a9a2e2
Peyman Eskandari <peyman.github@gmail.com>
2019-01-21 09:12:42 +0330

    cleanup index

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        7seas
-      </h1>
-      <h2 class="subtitle">
-        Seven Seas Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+
   </section>
 </template>
 
