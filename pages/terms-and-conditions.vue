8c813578a506a35588a60fa130d5fbea8dc26894
Peyman Eskandari <peyman.github@gmail.com>
2019-01-25 22:03:47 +0330

    add terms

---
+++ pages/terms-and-conditions.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div>
+
+    <div id="page-head">
+      <div class="title">Terms and Conditions</div>
+      <img src="@/assets/images/slide.jpg" class="bg">
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-8 col-center">
+
+          <!-- Page content -->
+          <div class="content">
+
+            <p>We thank you for having retained our services or for having considered retaining our services. For the sake of good order, please take note of the below TERMS and CONDITIONS of our SERVICES. If the same are not agreed please cancel any instructions you may have given us or refrain from instructing us. Cancellation should be made in writing.</p>
+            <p>Our terms of engagement and terms of services warranted binding on clients are:</p>
+            <p>Our services are rendered on the basis of normal diligence only and any report or advice is intended for the sole use of the person or organisation to whom it is addressed and no liability of any nature whatsoever shall be assumed to any other party in respect of its contents.</p>
+            <p>As to the client or the addressee, neither the Company nor the surveyor or directors, servants or employee or signatory shall be liable for any loss or damage whatsoever suffered by virtue of any act, omission or default (whether arising by negligence or otherwise) by the signatory, the Company or any of its servants.</p>
+            <p>We will use our experience, care and skills in fulfilling your instructions to the best of our ability.</p>
+            <p>We shall not be liable for any loss, injury, or damage of any nature whatsoever whether direct or consequential (including breach of confidentiality) arising out of or in connection with any service provided by us.</p>
+            <p>Any dispute shall be referred to Arbitration and Appeal before the competent Cypriot Courts under the laws of Cyprus.</p>
+            <p>The overall limit of liability in case same would be retained cannot exceed the level of fees received by the company. The client and we agree that this maximum amount is fair in view of the risks we assume in the performance of our services compared to the fees we will receive.</p>
+            <p>If you do not read or understand English it is your responsibility to advise us or to refrain from instructing us. You can reach us at +357 25 818 020 or at
+              <a href="mailto:ho@7seas.be">ho@7seas.be</a> for any advice you may need on the above.</p>
+
+            <p></p>
+            <p>
+              Seven Seas & Co Ltd. <br>
+              Directors <br>
+              Issued in Limassol on 30<sup>th</sup> June 2009 <br>
+            </p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
