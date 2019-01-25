dd8fa8a25eeb57f6de9a4b6747067ae75607ef07
Peyman Eskandari <peyman.github@gmail.com>
2019-01-25 22:03:52 +0330

    add hull

---
+++ pages/hull.vue
@@ -0,0 +1,58 @@
+<template>
+
+  <div>
+
+    <div id="page-head">
+      <div class="title">Hull</div>
+      <img src="@/assets/images/slide.jpg" class="bg">
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-8 col-center">
+
+          <!-- Page content -->
+          <div class="content">
+
+            <p>Available 24 hours a day and 365 days a year, the Hull Department offers all services related to:</p>
+            <p>General & Particular Average</p>
+
+            <div class="row">
+
+              <div class="col">
+                <ul>
+                  <li>Loss of hire, earnings</li>
+                  <li>Collision</li>
+                  <li>Salvage & towage</li>
+                  <li>Fixed and floating objects</li>
+                  <li>Casualty management and defence</li>
+                  <li>Insurance policy advice and pre-contracting guidance</li>
+                </ul>
+              </div>
+
+              <div class="col">
+                <ul>
+                  <li>Freight demurrage & defence</li>
+                  <li>Charter party disputes</li>
+                  <li>Transport law consultancy</li>
+                  <li>Drafting of commercial and shipping contracts</li>
+                  <li>Arbitration proceedings</li>
+                  <li>Litigation management</li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
