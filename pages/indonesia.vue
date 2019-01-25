e9601742bb821ddf1d1965e473a590bec64b3b0b
Peyman Eskandari <peyman.github@gmail.com>
2019-01-25 22:37:35 +0330

    add indonesia page

---
+++ pages/indonesia.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div>
+
+    <div id="page-head">
+      <div class="title">Announcement - Indonesia</div>
+      <img src="@/assets/images/slide.jpg" class="bg">
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-8 col-center">
+
+          <!-- Page content -->
+          <div class="content">
+
+            <div class="d-f justify-content-between align-items-center">
+
+              <div><img src="@/assets/images/mandiri-logo.png" alt="Mandiri Adjusting"></div>
+
+              <div><img src="@/assets/images/7seas-logo.png" alt="Seven Seas & Co"></div>
+
+            </div>
+
+            <br>
+            <br>
+
+            <p>Mandiri Adjusting and Seven Seas & Co‎ are delighted to announce that they have agreed a joint working arrangement which comes into effect on 24 February 2017.</p>
+            <p>Mandiri Adjusting is a new, first-rate adjusting business based in Indonesia. Operating with experienced and expert loss adjusters, Mandiri handle all kinds of Marine classes of insurance claims surveys and loss-adjusting investigations.</p>
+            <p>Seven Seas & Co has significantly expanded its business in the Asia Pacific Region. They now cover all the Belgium ports, Cyprus, France, Côte d’Ivoire, Poland, Russia, Senegal, Turkey, Angola, DR of the Congo, Morocco, São Tomé e Principe and now have a reinforced presence in Indonesia.</p>
+            <p>Indonesia has a fast-growing economy, which is comprised of so many islands/ports with very considerable coastal maritime traffic offering the insurance Market opportunities in marine risks.</p>
+            <p>Mandiri Adjusting will provide local, specialist adjusting in Indonesia and Seven Seas & Co will offer supplementary technical expertise and its global reputation.</p>
+
+            <p>For further information please contact:</p>
+            <p>Seven Seas & Co - Marc Dekeukelaere at <a href="mailto:mdk@7seas.bg">mdk@7seas.bg</a></p>
+            <p>Website: <a href="http://7seas.be">www.7seas.be</a></p>
+            <p>Mandiri Adjusting - Meru Andriyasto at
+              <a href="mailto:Indonesia@7seas.bg">Indonesia@7seas.bg</a> or
+              <a href="mailto:meru@mnt.co.id">meru@mnt.co.id</a></p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
