32931fdd5d6663755c7a3261f9da1c7b3c9fb556
Peyman Eskandari <peyman.github@gmail.com>
2019-01-25 20:20:42 +0330

    add menu to config

--- config/general.js
+++ config/general.js
@@ -7,6 +7,57 @@ export default {
   header_title: 'Average adjusters - Marine surveyors - Claims and recovery agents <br> Transport lawyers - Marine consultants and brokers',
 
   // Copyright
-  copyright: 'All rights reserved. 2019'
+  copyright: 'All rights reserved. 2019',
+
+  // Menu
+  menu: [
+
+    {
+      title: 'Home',
+      to: '/',
+    },
+    {
+      title: 'Hull',
+      to: '/page/hull',
+    },
+    {
+      title: 'Cargo',
+      to: '/page/cargo',
+    },
+    {
+      title: 'Our Team',
+      to: '/team',
+    },
+    {
+      title: 'Our Clients',
+      to: '/page/clients',
+    },
+    {
+      title: 'Africa',
+      children: [
+        {
+          title: 'Angola',
+          to: '/page/angola',
+        },
+        {
+          title: 'D.R. of the Congo',
+          to: '/page/congo',
+        },
+        {
+          title: 'Morocco',
+          to: '/page/morocco',
+        },
+      ]
+    },
+    {
+      title: 'Indonesia',
+      to: '/page/indonesia',
+    },
+    {
+      title: 'Terms and Conditions',
+      to: '/page/terms-and-conditions',
+    },
+
+  ]
 
 };
