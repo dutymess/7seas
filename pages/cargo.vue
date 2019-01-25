10954479360b7c222ea5fa3a64039960926c0c22
Peyman Eskandari <peyman.github@gmail.com>
2019-01-25 22:07:55 +0330

    add cargo page

---
+++ pages/cargo.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div>
+
+    <div id="page-head">
+      <div class="title">Cargo</div>
+      <img src="@/assets/images/slide.jpg" class="bg">
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-8 col-center">
+
+          <!-- Page content -->
+          <div class="content">
+
+            <p>Available 24 hours a day and 365 days a year, the Cargo Department offers the following services to cargo and insurance interests in respect of: </p>
+
+            <ul>
+              <li>Loss prevention and risk assessment</li>
+              <li>
+                After the casualty occurred the department offers:
+                <ul>
+                  <li>Survey arrangements worldwide, in house or through correspondents</li>
+                  <li>Safeguarding legal rights</li>
+                  <li>Loss mitigation</li>
+                  <li>Salvage sales</li>
+                  <li>Obtaining adequate security</li>
+                  <li>Ship Arrest Procedures</li>
+                  <li>World wide travel availability</li>
+                  <li>Adjustment of claims</li>
+                  <li>Recoveries, on a No Cure-No-Pay basis</li>
+                </ul>
+              </li>
+              <li>Management of major casualties</li>
+              <li>Salvage negotiations and provision of security</li>
+              <li>Alternative dispute resolution</li>
+              <li>Arbitration proceedings</li>
+              <li>Maritime fraud investigations</li>
+              <li>Theft and non-delivery of cargo</li>
+            </ul>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
