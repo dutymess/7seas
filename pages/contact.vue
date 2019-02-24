63a6af4b3430ad1607f88aa71324e84fb7426bc0
Peyman Eskandari <peyman.github@gmail.com>
2019-02-24 09:27:46 +0330

    replace indonesia with contact

---
+++ pages/contact.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div>
+
+    <div id="page-head">
+      <div class="title">Contact</div>
+      <img src="@/assets/images/slide.jpg" class="bg">
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-8 col-center">
+
+          <!-- Page content -->
+          <div class="content">
+
+            <p>Available 24 hours a day and 365 days a year:</p>
+            <ul>
+              <li>
+                <span>24/7 tel:</span>
+                <h4 style="margin-top: 8px;">+32 495 77 97 17</h4>
+              </li>
+              <li>
+                <span>Email:</span>
+                <h4 style="margin-top: 8px;"><a href="mailto:info@7seas.bg">info@7seas.bg</a></h4>
+              </li>
+            </ul>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
