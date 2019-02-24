777297dc9a0f20ea7a0e7e204242f5127b1583a5
Peyman Eskandari <peyman.github@gmail.com>
2019-02-24 15:35:35 +0330

    add services images

--- config/services.js
+++ config/services.js
@@ -3,32 +3,32 @@
 -----------------------------------------------------------------*/
 export default [
   {
-    img: 'service-1',
+    img: 'adjustment',
     title: 'Average Adjusting',
     desc: 'Hull & Machinery - Cargo - General Average',
   },
   {
-    img: 'service-1',
+    img: 'surveys',
     title: 'Surveys',
     desc: 'Marine - Cargo - Loss Prevention',
   },
   {
-    img: 'service-1',
+    img: 'recoveries',
     title: 'Recoveries',
     desc: 'Under subrogation or below deductible - On behalf of transport players',
   },
   {
-    img: 'service-1',
+    img: 'marine-services',
     title: 'Marine Services',
     desc: 'Ship Registration - Classification - Sale & Purchase - Cargo, Hull and P&I Insurance Brokers',
   },
   {
-    img: 'service-1',
+    img: 'emergencies',
     title: 'Emergencies',
     desc: 'Worldwide Problem Solving - Emergency Team - Ready To Travel 24/7',
   },
   {
-    img: 'service-1',
+    img: 'claim',
     title: 'Claims Management',
     desc: 'Global policies cargo - Charterers liability - Arbitrations - Charter party disputes - Fraud investigations',
   },
